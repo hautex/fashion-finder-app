ebcdbcd5c550c2288acba05387acc3dbb50eb288
hautex <hautex123@gmail.com>
2025-03-17 16:14:46 +0100

    Amélioration du service de recherche pour extraire les URLs directes des produits

--- backend/services/productSearch.js
+++ backend/services/productSearch.js
@@ -4,6 +4,7 @@
  */
 
 const axios = require('axios');
+const directProductSearch = require('./directProductSearch');
 
 // Sites de shopping fiables pour chaque catégorie
 const TRUSTED_FASHION_SITES = {
@@ -37,6 +38,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Bottines Chelsea en cuir brun - Clarks Desert Chelsea',
       link: 'https://www.clarks.fr/c/desert-chelsea-2/26078358.html',
+      directLink: 'https://www.clarks.fr/c/desert-chelsea-2/26078358.html',
       displayLink: 'www.clarks.fr',
       image: 'https://clarks.scene7.com/is/image/Pangaea2Build/26078358_W_1?wid=2000&hei=2000&fmt=jpg',
       snippet: 'Bottines Chelsea en cuir marron premium, semelle en caoutchouc, doublure en cuir respirant.',
@@ -45,6 +47,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Bottines à lacets marron - Timberland Premium 6-inch',
       link: 'https://www.timberland.fr/shop/fr/tbl-fr/6-inch-premium-boots-pour-homme-en-marron-10001',
+      directLink: 'https://www.timberland.fr/shop/fr/tbl-fr/6-inch-premium-boots-pour-homme-en-marron-10001',
       displayLink: 'www.timberland.fr',
       image: 'https://images.timberland.com/is/image/timberland/10001713-HERO?$PDP-FULL-IMAGE$',
       snippet: 'Bottines iconiques en cuir imperméable marron, col rembourré, semelle anti-fatigue.',
@@ -53,6 +56,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Bottines cuir marron - Red Wing Iron Ranger',
       link: 'https://www.redwingshoes.com/heritage/mens/iron-ranger/iron-ranger-08111.html',
+      directLink: 'https://www.redwingshoes.com/heritage/mens/iron-ranger/iron-ranger-08111.html',
       displayLink: 'www.redwingshoes.com',
       image: 'https://embed.widencdn.net/img/redwing/bexlfsgl2x/600x600px/RW08111_MUL_N1_0181_2_3.jpeg?position=s&crop=no&color=EDE8DD',
       snippet: 'Bottines artisanales en cuir huilé marron, bout renforcé, semelle Vibram durable.',
@@ -61,6 +65,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Boots en cuir marron - Dr. Martens 1460',
       link: 'https://www.drmartens.com/fr/fr/1460-smooth-leather-lace-up-boots/p/11822212',
+      directLink: 'https://www.drmartens.com/fr/fr/1460-smooth-leather-lace-up-boots/p/11822212',
       displayLink: 'www.drmartens.com',
       image: 'https://i1.adis.ws/i/drmartens/11822212.80.jpg?$medium$',
       snippet: 'Boots emblématiques en cuir lisse marron, 8 œillets, coutures jaunes, semelle AirWair.',
@@ -69,6 +74,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Chelsea Boots Marron - Selected Homme',
       link: 'https://www.zalando.fr/selected-homme-slhlouis-chelsea-boot-bottines-marron-se622d0dy-o11.html',
+      directLink: 'https://www.zalando.fr/article/se622d0dy-o11',
       displayLink: 'www.zalando.fr',
       image: 'https://img01.ztat.net/article/spp-media-p1/a6197bafdc5a30a0aae695ef968a5816/ab0df851ee7a48ebb7a4e337997723e3.jpg?imwidth=1800',
       snippet: 'Chelsea boots élégants en cuir marron, élastiques latéraux, bout légèrement pointu.',
@@ -81,6 +87,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Sacoche en cuir marron - The Bridge Story Uomo',
       link: 'https://www.thebridgeonlineshop.com/fr/pc_sacoches_the_bridge_story_uomo_marron_cuir_retro_06460001-14.html',
+      directLink: 'https://www.thebridgeonlineshop.com/fr/pc_sacoches_the_bridge_story_uomo_marron_cuir_retro_06460001-14.html',
       displayLink: 'www.thebridgeonlineshop.com',
       image: 'https://www.thebridgeonlineshop.com/images/products/xxlarge/06460001-14_1.jpg',
       snippet: 'Sacoche en cuir pleine fleur marron, design vintage avec finitions métal antique.',
@@ -89,6 +96,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Messenger Bag Marron - Fossil Buckner',
       link: 'https://www.fossil.com/fr-fr/products/buckner-messenger/MBG9374200.html',
+      directLink: 'https://www.fossil.com/fr-fr/products/buckner-messenger/MBG9374200.html',
       displayLink: 'www.fossil.com',
       image: 'https://fossil.scene7.com/is/image/FossilPartners/MBG9374200_main?$sfcc_fos_large$',
       snippet: 'Sacoche en cuir marron avec fermoir magnétique, poches intérieures, bandoulière ajustable.',
@@ -97,6 +105,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Sac à bandoulière cuir marron - Lancaster Soft Vintage',
       link: 'https://www.lancaster.fr/fr/messenger-homme/soft-vintage-messenger-homme-marron-fonce.html',
+      directLink: 'https://www.lancaster.fr/fr/messenger-homme/soft-vintage-messenger-homme-marron-fonce.html',
       displayLink: 'www.lancaster.fr',
       image: 'https://www.lancaster.fr/media/catalog/product/s/o/soft-vintage-3-messenger-homme-marron-fonce-3.jpg',
       snippet: 'Sacoche horizontale en cuir de vachette, format A4, fermeture zippée, doublure coton.',
@@ -105,6 +114,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Sacoche bandoulière marron - Tommy Hilfiger Essential',
       link: 'https://fr.tommy.com/sacoche-bandouliere-en-cuir-essential-am0am08535gae',
+      directLink: 'https://fr.tommy.com/sacoche-bandouliere-en-cuir-essential-am0am08535gae',
       displayLink: 'fr.tommy.com',
       image: 'https://tommy-europe.scene7.com/is/image/TommyEurope/AM0AM08535_GAE_main?$main@2x$',
       snippet: 'Sacoche compacte en cuir marron avec logo métallique, compartiment principal zippé.',
@@ -113,6 +123,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Sac bandoulière cuir marron - Le Tanneur Bruno',
       link: 'https://www.letanneur.com/fr-fr/p/bruno-sac-bandouliere-homme-tltm2110',
+      directLink: 'https://www.letanneur.com/fr-fr/p/bruno-sac-bandouliere-homme-tltm2110',
       displayLink: 'www.letanneur.com',
       image: 'https://www.letanneur.com/media/catalog/product/t/l/tltm2110_899_1.jpg',
       snippet: 'Sac messager en cuir de vachette pleine fleur, format A5, poche extérieure zippée.',
@@ -125,6 +136,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Article Mode Tendance - Collection Actuelle',
       link: 'https://www.zalando.fr/mode/',
+      directLink: 'https://www.zalando.fr/mode/',
       displayLink: 'www.zalando.fr',
       image: 'https://img01.ztat.net/article/spp-media-p1/7df308f9c58a3f488652317f6786ee72/dd02f2e6e2a245b0a89c61a113d56a96.jpg',
       snippet: 'Découvrez les dernières tendances mode, tous styles et toutes marques.',
@@ -133,6 +145,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Vêtements et Accessoires de Qualité',
       link: 'https://www2.hm.com/fr_fr/index.html',
+      directLink: 'https://www2.hm.com/fr_fr/index.html',
       displayLink: 'www2.hm.com',
       image: 'https://lp2.hm.com/hmgoepprod?source=url[https://www2.hm.com/content/dam/TOOLBOX/PRE_SEASON/2022_pss/March_2022/Startpage_1_1_Trend.jpg]&scale=size[960]&sink=format[jpeg],quality[80]',
       snippet: 'Mode femme, homme et enfant au meilleur prix, collections exclusives et nouvelles tendances.',
@@ -141,6 +154,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Nouveautés Mode - Les essentiels du moment',
       link: 'https://www.asos.com/fr/nouveautes/cat/?cid=27108',
+      directLink: 'https://www.asos.com/fr/nouveautes/cat/?cid=27108',
       displayLink: 'www.asos.com',
       image: 'https://images.asos-media.com/products/asos-design-ensemble-sweat-a-capuche-et-jogger-oversize-noir/204050837-1-black',
       snippet: 'Découvrez les nouveautés mode pour femme et homme, mises à jour quotidiennement. Livraison et retours gratuits.',
@@ -149,6 +163,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Collection Automne-Hiver - Vêtements tendance',
       link: 'https://www.zara.com/fr/',
+      directLink: 'https://www.zara.com/fr/',
       displayLink: 'www.zara.com',
       image: 'https://static.zara.net/photos///2022/I/0/1/p/7901/423/712/2/w/850/7901423712_1_1_1.jpg',
       snippet: 'Découvrez les nouvelles collections de vêtements, chaussures et accessoires pour femme, homme et enfant.',
@@ -157,6 +172,7 @@ const FALLBACK_PRODUCTS = {
     {
       title: 'Mode éthique et responsable - Vêtements durables',
       link: 'https://www.veja-store.com/fr/',
+      directLink: 'https://www.veja-store.com/fr/',
       displayLink: 'www.veja-store.com',
       image: 'https://www.veja-store.com/media/catalog/product/cache/4d7748d1b22d0fb94ae7e27ea11d5a07/v/e/veja_v-10_cwl_white_natural_cobalt_purple_vx022426a_lateral.jpg',
       snippet: 'Marque pionnière de la mode responsable, matières biologiques et commerce équitable.',
@@ -194,7 +210,7 @@ async function searchFashionProducts(query, itemType = 'default', color = '') {
     const sitesQuery = trustedSites.map(site => `site:${site}`).join(' OR ');
     
     // Ajouter des termes spécifiques au shopping
-    const shoppingTerms = 'acheter prix boutique vente';
+    const shoppingTerms = 'acheter prix boutique vente produit';
     
     // Construire la requête finale
     const enhancedQuery = `(${query}) (${sitesQuery}) ${shoppingTerms}`;
@@ -214,7 +230,10 @@ async function searchFashionProducts(query, itemType = 'default', color = '') {
       q: enhancedQuery,
       num: 10, // Demander plus pour pouvoir filtrer
       lr: 'lang_fr', // Limiter aux résultats en français
-      safe: 'active' // Filtre SafeSearch
+      safe: 'active', // Filtre SafeSearch
+      searchType: 'shopping', // Orienter vers les résultats de shopping si possible
+      siteSearchFilter: 'e', // Inclure seulement les sites spécifiés
+      sort: '' // Laisser le moteur de recherche trier par pertinence
     };
     
     // URL de recherche
@@ -231,17 +250,29 @@ async function searchFashionProducts(query, itemType = 'default', color = '') {
     
     console.log(`Nombre de résultats bruts trouvés: ${response.data.items.length}`);
     
-    // Traitement et filtrage des résultats
-    let processedResults = response.data.items.map(item => ({
-      title: item.title || 'Produit sans titre',
-      link: item.link || '',
-      displayLink: item.displayLink || '',
-      image: item.pagemap?.cse_image?.[0]?.src || 
-             item.pagemap?.cse_thumbnail?.[0]?.src || 
-             getPlaceholderImage(itemType, color),
-      snippet: item.snippet || '',
-      price: extractPrice(item.title, item.snippet)
-    }));
+    // Traitement et filtrage des résultats - AVEC EXTRACTION D'URL DIRECTE
+    let processedResults = response.data.items.map(item => {
+      // Vérifier si c'est une URL de produit
+      const isProduct = directProductSearch.isProductUrl(item.link);
+      
+      // Extraire l'URL directe vers le produit
+      const directLink = isProduct 
+        ? directProductSearch.extractDirectProductUrl(item.link)
+        : item.link;
+      
+      return {
+        title: item.title || 'Produit sans titre',
+        link: item.link || '',
+        directLink: directLink, // Nouvelle propriété avec lien direct
+        displayLink: item.displayLink || '',
+        image: item.pagemap?.cse_image?.[0]?.src || 
+               item.pagemap?.cse_thumbnail?.[0]?.src || 
+               getPlaceholderImage(itemType, color),
+        snippet: item.snippet || '',
+        price: extractPrice(item.title, item.snippet),
+        isDirectProductLink: isProduct // Indique si le lien mène directement à un produit
+      };
+    });
     
     // Filtrer les résultats sans image ou avec des liens invalides
     processedResults = processedResults.filter(item => 
@@ -249,6 +280,15 @@ async function searchFashionProducts(query, itemType = 'default', color = '') {
       !item.link.includes('recherche') && // Éviter les pages de recherche
       !item.link.includes('search')
     );
+    
+    // Filtrer pour favoriser les liens directs vers les produits
+    if (processedResults.some(item => item.isDirectProductLink)) {
+      // Si on a des liens directs, on les privilégie
+      const directProducts = processedResults.filter(item => item.isDirectProductLink);
+      if (directProducts.length >= 3) {
+        processedResults = directProducts;
+      }
+    }
     
     // Essayer d'enrichir l'information de prix si manquante
     processedResults = processedResults.map(item => {
@@ -333,11 +373,11 @@ function extractPrice(title, snippet) {
   
   // Recherche plus robuste de prix en différentes devises
   // Capture €, £, $, suivi d'un espace optionnel, puis d'un nombre avec décimales optionnelles
-  const priceRegexEUR = /(€|EUR|euros?)\s?(\d+(?:[\.,]\d{2})?)/i;
-  const priceRegexUSD = /(\$|USD|dollars?)\s?(\d+(?:[\.,]\d{2})?)/i;
-  const priceRegexGBP = /(£|GBP|livres?)\s?(\d+(?:[\.,]\d{2})?)/i;
-  const priceRegexGeneric = /(\d+(?:[\.,]\d{2})?)\s?(€|EUR|USD|\$|£|GBP)/i;
-  const priceRegexSimple = /(\d+(?:[\.,]\d{2})?)(?:\s?€)/i;
+  const priceRegexEUR = /(€|EUR|euros?)\s?(\d+(?:[.,]\d{2})?)/i;
+  const priceRegexUSD = /(\$|USD|dollars?)\s?(\d+(?:[.,]\d{2})?)/i;
+  const priceRegexGBP = /(£|GBP|livres?)\s?(\d+(?:[.,]\d{2})?)/i;
+  const priceRegexGeneric = /(\d+(?:[.,]\d{2})?)\s?(€|EUR|USD|\$|£|GBP)/i;
+  const priceRegexSimple = /(\d+(?:[.,]\d{2})?)(?:\s?€)/i;
   
   // Essayer tous les formats de prix
   const matchEUR = combined.match(priceRegexEUR);
